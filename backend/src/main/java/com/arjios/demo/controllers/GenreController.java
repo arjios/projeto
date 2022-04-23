5c0e828390d449cefbfab9f2a822f02442988b44
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-04-23 16:50:07 -0300

    Role and Genre Controllers

--- backend/src/main/java/com/arjios/demo/controllers/GenreController.java
+++ backend/src/main/java/com/arjios/demo/controllers/GenreController.java
@@ -9,26 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.arjios.demo.dto.RoleDTO;
-import com.arjios.demo.services.RoleService;
+import com.arjios.demo.dto.GenreDTO;
+import com.arjios.demo.services.GenreService;
 
 @RestController
-@RequestMapping(value="/roles")
-public class RoleController {
+@RequestMapping(value="/genres")
+public class GenreController {
 	
 	@Autowired
-	private RoleService roleService;
+	private GenreService genreService;
 	
 	@GetMapping
-	public ResponseEntity<Page<RoleDTO>> findAll(Pageable pageable) {
-		Page<RoleDTO> page = roleService.findAll(pageable);
+	public ResponseEntity<Page<GenreDTO>> findAll(Pageable pageable) {
+		Page<GenreDTO> page = genreService.findAll(pageable);
 		return ResponseEntity.ok(page);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<RoleDTO> findById(@PathVariable Long id) {
-		RoleDTO roleDTO = roleService.findById(id);
-		return ResponseEntity.ok().body(roleDTO);
+	public ResponseEntity<GenreDTO> findById(@PathVariable Long id) {
+		GenreDTO genreDTO = genreService.findById(id);
+		return ResponseEntity.ok().body(genreDTO);
 	}
 
 }
